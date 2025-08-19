f0b81c1a5117aeda3898b52b86aa1ef3b582d74e
joaovictorino <jhvictorino@gmail.com>
2025-08-19 17:04:18 -0300

    ajustes de nomenclatura

--- terraform/05. upload_image.tf
+++ terraform/05. upload_image.tf
@@ -1,8 +1,12 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
 resource "null_resource" "upload_image" {
   triggers = {
     order = aws_ecr_repository.springapp.id
   }
   provisioner "local-exec" {
-    command = "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 475154562783.dkr.ecr.us-east-1.amazonaws.com && docker push 475154562783.dkr.ecr.us-east-1.amazonaws.com/springapp:latest && sleep 20"
+    command = "aws ecr get-login-password --region ${data.aws_region.current.name} | docker login --username AWS --password-stdin ${aws_ecr_repository.springapp.repository_url} && docker push ${aws_ecr_repository.springapp.repository_url}:latest && sleep 20"
   }
 }
